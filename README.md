7483c2456259f391947e96b6ec6d90245bfb5775
ChristopherNoelGoldsmith <ChristopherGoldsmith101@yahoo.com>
2022-05-08 08:39:06 -0700

    immediate compatability with pages added without the need for a div or css

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cascading-bubbles
+# Cascading Bubbles
+
+A simple script which adds a bubble effect to the background of your page.
+
+Default value of the z-index if set a -100.
+
+Bubbles will raise form the bottom of the page every 500ms and will have varied durations and sizes. All bubbles will be removed from the dom upon the end of thei animation duration.
+
+Script is written in vanilla javascript.
+
+ReactJS version in progress...
--- bubbles.js
+++ bubbles.js
@@ -1,10 +1,10 @@
-/*For this animation to function all you need do is add a div onto the dom with the id 'bubbles */
+(function () {
+    const createKeyframes = () => {
 
-const createKeyframes = () => {
-
-    //inserts keyframes and positions the bubble div on the DOM.
-    const css = document.styleSheets[0];
-    css.insertRule(`
+        //inserts keyframes and positions the bubble div on the DOM.
+        const css = document.styleSheets[0];
+        console.log(document.styleSheets)
+        css.insertRule(`
     @keyframes bubble-up {
         from {
             transform: translateY(0%);
@@ -15,43 +15,43 @@ const createKeyframes = () => {
             opacity: 0;
         }
     }`, css.cssRules.length)
-    css.insertRule(`
+        css.insertRule(`
     #bubbles {
         position: fixed;
         bottom: 0px;
         left: 0;
         height: 120vh;
     }`, css.cssRules.length)
-};
-
-const cascadingBubbles = () => {
-    //targets dom element creates bubble then appends it.
-    const bubble = document.createElement('div');
-    //determines the size of the bubble and its animation duration.
-    const bubbleSize = Math.round(Math.random() * 100);
-    const duration = Math.round(Math.random() * 20) + 5;
-    const position = Math.floor(Math.random() * 85) + 5;
-    //const index = Math.round(Math.random() * 2000) - 100;
-    //Gives the bubble a class to it can be selected.
-    //Create css for the bubble.
-    bubble.style.zIndex = `10000`;
-    bubble.style.animation = `bubble-up ${duration}s ease-out`;
-    bubble.style.position = `fixed`;
-    bubble.style.left = `${position}%`;
-    bubble.style.height = `${bubbleSize}px`;
-    bubble.style.width = `${bubbleSize}px`;
-    bubble.style.background = `rgba(255, 255, 255, 0.2)`;
-    bubble.style.border = `1px solid white`;
-    bubble.style.borderRadius = `50%`;
-    bubble.style.bottom = `-200px`;
-    document.getElementById('bubbles').appendChild(bubble);
-
-    const removeBubble = () => {
-        bubble.parentNode.removeChild(bubble);
     };
-    setTimeout(cascadingBubbles, 500)
-    setTimeout(removeBubble, (duration * 1000))
-};
 
-createKeyframes();
-cascadingBubbles();
+    const cascadingBubbles = () => {
+        //targets dom element creates bubble then appends it.
+        const bubble = document.createElement('div');
+        //determines the size of the bubble and its animation duration.
+        const bubbleSize = Math.round(Math.random() * 100);
+        const duration = Math.round(Math.random() * 20) + 5;
+        const position = Math.floor(Math.random() * 85) + 5;
+
+        //styles of the bubbles
+        //bubble.id = `${Math.round(bubbleSize + position)}POPPING!`
+        bubble.style.zIndex = `-100`;
+        bubble.style.animation = `bubble-up ${duration}s ease-out`;
+        bubble.style.position = `fixed`;
+        bubble.style.left = `${position}%`;
+        bubble.style.height = `${bubbleSize}px`;
+        bubble.style.width = `${bubbleSize}px`;
+        bubble.style.background = `rgba(255, 255, 255, 0.2)`;
+        bubble.style.border = `1px solid white`;
+        bubble.style.borderRadius = `50%`;
+        bubble.style.bottom = `-200px`;
+        document.querySelector('body').appendChild(bubble);
+
+        const removeBubble = () => {
+            bubble.parentNode.removeChild(bubble);
+        };
+        setTimeout(cascadingBubbles, 500)
+        setTimeout(removeBubble, (duration * 1000))
+    };
+    createKeyframes();
+    cascadingBubbles();
+})()
